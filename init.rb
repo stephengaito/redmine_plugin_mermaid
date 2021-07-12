e9d2de247cb9445f3955c888fdab223acd27461c
ocamler <1463721+ocamler@users.noreply.github.com>
2021-07-12 14:00:23 +0000

    got plugin to work in Preview mode; updated Mermaid version to mermaid-js/mermaid@52a4011

--- init.rb
+++ init.rb
@@ -6,14 +6,20 @@ Redmine::Plugin.register :redmine_mermaid do
   name 'Redmine Mermaid plugin'
   author 'Yuji Sato'
   description 'This is a mermaid.js plugin for Redmine'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/styz/redmine_mermaid'
   author_url 'https://github.com/styz'
 
   Redmine::WikiFormatting::Macros.register do
     desc 'Wiki/Issues mermaid'
     macro :mermaid do |obj, args, text|
-      content_tag('div', text, :class => 'mermaid')
+      html = content_tag('div', text, :class => 'mermaid')
+      html += javascript_tag(
+	      "$('div.mermaid:not([data-processed])').each(" +
+	      "  function () { mermaid.init(undefined, this); }" +
+	      ");"
+      )
+      html
     end
   end
 end
