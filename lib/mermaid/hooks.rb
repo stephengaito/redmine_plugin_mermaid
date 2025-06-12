6989f97df1a9166b9f34de6dd3a1780f22f8aed0
Stephen Gaito <stephen@perceptisys.co.uk>
2025-06-12 10:57:54 +0100

    another try

--- lib/mermaid/hooks.rb
+++ lib/mermaid/hooks.rb
@@ -1,7 +1,7 @@
 # see: https://mermaid.js.org/intro/#mermaid-api
 
 module RedmineMermaid
-  class Hook < Redmine::Hook::ViewListener
+  class Hooks < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context = {})
       tags = javascript_include_tag 'mermaid.min.js', plugin: 'mermaid'
       # tags += stylesheet_link_tag 'mermaid.css', plugin: 'mermaid'
